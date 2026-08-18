c02afae82a99a863aba9c1c4d2053732b502fc7b
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2026-08-18 19:48:58 +0900

    typos and sidebar fixed

--- src/components/content/PageMonitorarAvaliar.jsx
+++ src/components/content/PageMonitorarAvaliar.jsx
@@ -22,7 +22,7 @@ const PageMonitorarAvaliar = () => {
     <div ref={ref} id="monitorar-avaliar" className="scroll-mt-20 p-8 bg-white dark:bg-slate-900 rounded-2xl shadow-2xl mx-auto max-w-5xl space-y-8">
 
       <h2 className={`${titleFont.className} text-4xl font-bold text-center text-slate-600 dark:bg-none dark:text-white`}>
-        Obetivos do Módulo 3
+        Objetivos do Módulo 3
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
